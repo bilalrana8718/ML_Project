73ec44bc41614e8eabb1daf635fb9782b4dd191f
Rana Bilal Akbar <bilal.rana8718@gmail.com>
2024-10-05 20:03:23 +0500

    Exceptions and LOG

--- src/logger.py
+++ src/logger.py
@@ -2,7 +2,7 @@ import logging
 import os
 from datetime import datetime
 
-LOG_FILE= f"{datetimme.now().strftime('%m_%d_%Y_%H_%S')}.log"
+LOG_FILE= f"{datetime.now().strftime('%m_%d_%Y_%H_%S')}.log"
 
 logs_path = os.path.join(os.getcwd(), "logs", LOG_FILE)
 
@@ -12,6 +12,6 @@ LOG_FILE_PATH=os.path.join(logs_path,  LOG_FILE)
 
 logging.basicConfig(
     filename=LOG_FILE_PATH,
-    format="[ %(asctime)s ] %%(lineno)d %(name)s - %(levelname)s - %(message)s",
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO,
 )
