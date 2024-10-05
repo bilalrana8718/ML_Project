22fd8a8744348303eec92cd05a2df8c5f6a1d77a
Rana Bilal Akbar <bilal.rana8718@gmail.com>
2024-10-06 02:12:23 +0500

    Data Update

--- src/exception.py
+++ src/exception.py
@@ -15,5 +15,5 @@ class CustomException(Exception):
         super().__init__(error_message)
         self.error_message_detail = error_message_details(error_message, error_message_detail)
 
-    def __str__(self):
+    def __str__(self): 
         return self.error_message 
